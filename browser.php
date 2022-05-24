dafafe66372bff6ead940b0eaa3bbc61f6c5336b
romaneneelia <romaneneelia@gmail.com>
2022-05-24 17:37:04 +0200

    browser

--- browser.php
+++ browser.php
@@ -1,18 +1,8 @@
 <?php
     include 'header.php';
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
-    <link rel="stylesheet" href="styles1.css">
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-  
-</head>
-<body>
+
+<div class="wrapper">
     <div class="container">
         <div class="container-onglets">
             <div class="onglets active" data-anim="1">Médecine générale</div>
@@ -41,8 +31,8 @@
     </div>
 
     <script src="app.js"></script>
-</body>
 
+</div>
 
   
 
--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="headerstyle.css">
+    <link rel="stylesheet" href="styles1.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <title>Test hébergement</title>
 
