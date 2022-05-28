deb095583d9b589d64785beb7f02cbb1437d6d7b
romaneneelia <romaneneelia@gmail.com>
2022-05-28 16:56:38 +0200

    accueil

--- header.php
+++ header.php
@@ -7,8 +7,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="footer.css">
     <link rel="stylesheet" href="headerstyle.css">
+    <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="styles1.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
